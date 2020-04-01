8cf4ede4d62fc705061a4b7ee4350070f31415e2
github username <github email address>
2020-04-01 16:18:06 -0400

    filtered

--- src/components/App.js
+++ src/components/App.js
@@ -6,14 +6,37 @@ import HogContainer from "./HogContainer";
 
 class App extends Component {
   state = {
-    hogsArray: hogs
+    hogsArray: hogs,
+    filteredByGrease: false
   }
 
+  filterByGrease = () => {
+    this.setState({
+      filteredByGrease: !this.state.filteredByGrease
+    })
+  
+    if (this.state.filteredByGrease === true) {
+      let newArr = this.state.hogsArray.filter
+      (hog => hog.greased === true )
+      this.setState({
+        hogsArray: newArr
+      })
+      // console.log('true')
+    } else {
+      this.setState({
+        hogsArray: hogs
+      })
+      // console.log('false')
+    }
+    // () => console.log(this.state.filteredByGrease))
+
+  }
   render() {
     return (
       <div className="App">
         <Nav />
-        <HogContainer hogsArray={this.state.hogsArray}/>
+        <HogContainer hogsArray={this.state.hogsArray} filterByGrease = {this.filterByGrease}/>
+
       </div>
     );
   }
--- src/components/HogContainer.js
+++ src/components/HogContainer.js
@@ -4,7 +4,10 @@ import HogTile from './HogTile';
 class HogContainer extends React.Component {
   render() {
     return (
+            
       <div>
+        <button onClick = {this.props.filterByGrease} >filter by grease</button>
+
         {this.props.hogsArray.map(hog => {
           return <HogTile hogStuff={hog} />
         })}
